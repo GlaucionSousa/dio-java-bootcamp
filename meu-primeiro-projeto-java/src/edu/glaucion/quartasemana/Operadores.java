6cd8e7541948491f73e30af676dd022b74a9419f
GlaucionSousa <sousaglaucion@outlook.com>
2024-05-31 15:46:33 -0300

    Operadores

---
+++ meu-primeiro-projeto-java/src/edu/glaucion/quartasemana/Operadores.java
@@ -0,0 +1,24 @@
+package edu.glaucion.quartasemana;
+
+public class Operadores {
+    public static void main(String[] args) {
+        //classe Operador de atribuição
+        String nome = "GLEYSON";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date();
+
+        //classe Operador aritmetico
+        double soma = 10.5 + 15.7;
+        int subtração = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20/4);
+        //O operador de adição (+), quando utilizado
+        //em variáveis do tipo texto, realizará a “concatenação de textos”.
+        //String nomeCompleto = "LINGUAGEM" + "JAVA";
+    }
+}
