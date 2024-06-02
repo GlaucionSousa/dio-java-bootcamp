e20c4efba05e240ddb028e9db3d134ab26566a25
GlaucionSousa <sousaglaucion@outlook.com>
2024-06-02 15:44:58 -0300

    Update metodos!

--- meu-primeiro-projeto-java/src/smarttv/Usuario.java
+++ meu-primeiro-projeto-java/src/smarttv/Usuario.java
@@ -7,21 +7,20 @@ public class Usuario {
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
+        System.out.println("Volume Atual: " + smartTv.volume);
         
         System.out.println("Canal Atual: " + smartTv.canal);
+
         smartTv.mudarCanal(13);
+
         System.out.println("Novo Canal: " + smartTv.canal);
 
-        System.out.println("Volume Atual: " + smartTv.volume);
-
-
         System.out.println("TV Ligada? " + smartTv.ligada);
-        System.out.println("Volume Atual: " + smartTv.volume);
-
+        
         smartTv.ligar();
-        System.out.println("Vovo Status -> TV Ligada? " + smartTv.ligada);
+        System.out.println("Novo Status -> TV Ligada? " + smartTv.ligada);
 
         smartTv.desligar();
-        System.out.println("Vovo Status -> TV Ligada? " + smartTv.ligada);
+        System.out.println("Novo Status -> TV Ligada? " + smartTv.ligada);
     }
 }
