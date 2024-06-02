ac8a2a6d1966d25911539f7ae944a973693e5c07
GlaucionSousa <sousaglaucion@outlook.com>
2024-06-02 14:29:44 -0300

    Operadores Update

--- meu-primeiro-projeto-java/src/edu/glaucion/quartasemana/Operadores.java
+++ meu-primeiro-projeto-java/src/edu/glaucion/quartasemana/Operadores.java
@@ -20,5 +20,26 @@ public class Operadores {
         //O operador de adição (+), quando utilizado
         //em variáveis do tipo texto, realizará a “concatenação de textos”.
         //String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        //classe Operadores.java
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if(numero1 > numero2)
+            System.out.print("Numero 1 maior que numero 2");
+
+        if(numero1 < numero2)
+            System.out.print("Numero 1 menor que numero 2");
+
+        if(numero1 >= numero2)
+            System.out.print("Numero 1 maior ou igual que numero 2");
+
+        if(numero1 <= numero2)
+            System.out.print("Numero 1 menor ou igual que numero 2");
+
+        if(numero1 != numero2)
+            System.out.print("Numero 1 é diferente de numero 2");
     }
 }
